aa718956f3fd276d632ef607e16098b5e5bc3e91
Andrew Short <andrewjshort@gmail.com>
2012-08-09 21:35:13 +1000

    Update the cms path constants to work outside the base path.

--- _config.php
+++ _config.php
@@ -4,8 +4,8 @@
  * - CMS_DIR: Path relative to webroot, e.g. "cms"
  * - CMS_PATH: Absolute filepath, e.g. "/var/www/my-webroot/cms"
  */
-define('CMS_DIR', 'cms');
-define('CMS_PATH', BASE_PATH . '/' . CMS_DIR);
+define('CMS_DIR', basename(__DIR__));
+define('CMS_PATH', __DIR__);
 
 /**
  * Register the default internal shortcodes.
